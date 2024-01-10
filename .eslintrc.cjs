93ee69f8b45fa70a13bef0015b4a430f2e1afed5
xielizhen <xielz@hrtps.com>
2024-01-10 14:01:16 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-empty': 'off',
+    'no-debugger': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: 'useRecoilCallback'
+      }
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-this-alias': 'off'
+  }
+};
---
+++ src/components/slider/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './index.scss';
+
+const Slider: React.FC = () => {
+  return <div className="slider"></div>;
+};
+
+export default Slider;
